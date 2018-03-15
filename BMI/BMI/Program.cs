cfb4daf55bda7f5289bdb844cc9f9c0b5b1dbcb9
m305-23\w401 <jjjj000123000@gmail.com>
2018-03-15 18:43:09 +0800

    第一次，現在是匯率計算

---
+++ BMI/BMI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rate = 0;
+            // 顯示匯率
+            Console.WriteLine("目前匯率值 : {0} ", rate);
+
+            // 輸入金額
+            Console.Write("請輸入烏干達幣金額 : ");
+            string input = Console.ReadLine();
+
+            // 換算
+            double total = double.Parse(input) * rate;
+
+            // 結果
+            Console.WriteLine("換算成鬼島幣金額 :{0}", +total);
+        }
+    }
+}
