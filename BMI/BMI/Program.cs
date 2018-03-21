d27618a72f46fb8ebadf6979b930238ce136b29d
789510215465 <37402245+789510215465@users.noreply.github.com>
2018-03-22 00:57:50 +0800

    Update Program.cs
    BMI 計算

--- BMI/BMI/Program.cs
+++ BMI/BMI/Program.cs
@@ -10,19 +10,48 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            double rate = 0;
+            // double rate = 0;
             // 顯示匯率
-            Console.WriteLine("目前匯率值 : {0} ", rate);
+            // Console.WriteLine("目前匯率值 : {0} ", rate);
 
             // 輸入金額
-            Console.Write("請輸入烏干達幣金額 : ");
-            string input = Console.ReadLine();
+            // Console.Write("請輸入烏干達幣金額 : ");
+            // string input = Console.ReadLine();
 
             // 換算
-            double total = double.Parse(input) * rate;
+            // double total = double.Parse(input) * rate;
 
             // 結果
-            Console.WriteLine("換算成鬼島幣金額 :{0}", +total);
+            // Console.WriteLine("換算成鬼島幣金額 :{0}", +total);
+            
+            // BMI = 體重(公斤) /身高*身高(公尺*公尺)
+            double height = 0 ;
+            double weight = 0 ;
+            double bmi = 0 ;
+            
+            Console.WriteLine("請輸入身高(公尺) : {0} ", height);
+            string height = Console.ReadLine();
+           
+            Console.WriteLine("請輸入體重(公斤) : {0} ", weight);
+            string weight = Console.ReadLine();
+           
+            
+            double h = double.Parse(height)*double.Parse(height);
+            double bmi = double.Parse(weight) / h;
+            
+            Console.WriteLine("你的BMI值為: {0}", + bmi);
+            Console.ReadLine();    
+                
+            if 18.5≦bmi＜24
+            
+            
+            
+            
+            
+            
+            
+            
+                
         }
     }
 }
